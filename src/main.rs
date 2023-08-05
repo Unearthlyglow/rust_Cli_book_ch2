c155608628f6d8acac52677fb11e4fbaa51615ee
Award <awardmalisi@gmail.com>
2023-08-05 10:48:57 -0500

    notes added for better understanding of the values_of_lossy method

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ use clap::{App, Arg};
 fn main() {
     let matches = App::new("echor")
         .version("0.1.0")
-        .author("Ken Youens-Clark <kyclark@gmail.com>")
+        .author("Award Malisi <awardmalisi@gmail.com>")
         .about("Rust echo")
         .arg(
             Arg::with_name("text")
@@ -21,8 +21,14 @@ fn main() {
         .get_matches();
 
     let text = matches.values_of_lossy("text").unwrap();
+
     let omit_newline = matches.is_present("omit_newline");
     print!("{}{}", text.join(" "), if omit_newline { "" } else { "\n" });
 
     println!("{:#?}", matches);
 }
+
+//Line 23 -
+//Values: An iterator for getting multiple values out of an argument.
+
+//I will use the ArgMatches::values_of_lossy function to get an Option<Vec<String>>. The Option::unwrap function will take the value out of Some<T> to get at the payload T. Because the text argument is required by clap, I know it will be impossible to have None; therefore, I can safely call Option::unwrap to get the Vec<String> value. The reason "text" is used with values_of_lossy is because of: Arg::with_name("text").
